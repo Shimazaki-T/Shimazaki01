73688507d9ab039b3b8e7b0e9bc9cabf0763f434
Shimazaki-T <65791693+Shimazaki-T@users.noreply.github.com>
2020-05-31 23:45:06 +0900

    Add files via upload

---
+++ JavaScriptPractice/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+console.log('Hello World');
+console.log(73+47%8);
+console.log(navigator.appCodeName);
+let name = "72";
+name = 78;
+console.log(name+33);
+const hage = "worthless";
+console.log(hage);
+console.log(`${hage}は${name}歳です。`);
+const number = 8;
+ if (number > 10){
+   console.log("私、失敗しないので。");
+ }
+const hope = 18;
+console.log(hope <10);
+ if(true){
+   console.log("私。");
+ } else{
+   console.log("俺。");
+ }
+
+const neko = 35;
+ if(neko >10){
+    console.log("nekoは10より大きい");
+ } else if(neko >=3){
+   console.log("nekoは3以上");
+ } else {
+   console.log("nekoほとんどいない");
+ }
+function EVENT1(){window.alert("うんこ！")}
+function EVENT2(){
+  document.open();
+  document.write("<p>");
+  document.write("貴様はうんこを踏んでしまったのだ。");
+  document.write("</p>");
+  document.close();
+}
+function EVENT3A(){document.bgColor='brown'};
+function EVENT3B(){document.bgColor='#ffffff'};
